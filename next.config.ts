4b4bbce0e67df56115f95a23d35a1e452d43c53d
Michael Essam <145896866+Michael-Esam@users.noreply.github.com>
2026-01-03 17:37:44 +0200

    Add remote image patterns to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.neon.tech', 
+      },
+      {
+        protocol: 'https',
+        hostname: '**.aws.neon.tech', 
+      },
+    ],
+  },
 };
 
 export default nextConfig;
